af5161c83cc1e6e38e9c3812057e01bc0caab322
luizanmeneses <luizanmeneses@gmail.com>
2026-03-06 15:49:19 +0000

    01-start-base/

---
+++ 01-start-base/Arguments/Arguments.java
@@ -0,0 +1,5 @@
+public class Arguments {
+    public static void main(String[] args) {
+        System.out.println("Você digitou " + args[0]); //args é argumento, um array de string.
+    }
+}
---
+++ 01-start-base/First Program/FirstProgram.java
@@ -0,0 +1,11 @@
+// Pacote - classe - método - instrução
+//package primeiro_programa; // Declaração do pacote, caso queira colocar numa pasta específica.
+
+public class FirstProgram { // Classe deve começar com letra maiúscula
+
+    public static void main(String[] args) { 
+        // Método principal: ponto de entrada do programa
+        System.out.println("Olá, Mundo!");
+    }
+
+}
